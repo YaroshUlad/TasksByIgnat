0c072f10b9bf739c2ef59e3b4b6f015c59b31b87
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-06-02 13:58:01 +0300

    add type for initial state in loadingReducer.ts

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,25 @@
-const initState = {
-
+type LoadingReducerType = {
+    loading: boolean
+}
+const initState: LoadingReducerType = {
+    loading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+const enum ACTION_TYPES {
+    SET_TRUE = 'SET_STATE_LOADING_TO_TRUE',
+    SET_FALSE = 'SET_STATE_LOADING_TO_FALSE',
+}
+
+
+export const loadingReducer = (state: LoadingReducerType = initState, action: any): any => { // fix any
     switch (action.type) {
         case '': {
             return state
         }
-        default: return state
+        default:
+            return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (): any => {
+} // fix any
