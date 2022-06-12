b3d73884a57a3ed8c81afa287bd54b65d215fbc3
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-06-12 20:01:37 +0300

    hw11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -3,8 +3,8 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+    const [value1, setValue1] = useState<number>(0)
+    const [value2, setValue2] = useState<number>(100)
 
     return (
         <div>
@@ -13,18 +13,25 @@ function HW11() {
 
             {/*should work (должно работать)*/}
             <div>
-                <span>{value1}</span>
+
                 <SuperRange
+                    onChangeRange={(value: number)=> setValue1(value)}
+                    value={value1}
                     // сделать так чтоб value1 изменялось
                 />
+                <span>{value1}</span>
             </div>
 
             <div>
-                <span>{value1}</span>
                 <SuperDoubleRange
+                    value={[value1,value2]}
+                    onChangeRange={([a,b])=>{
+                        setValue1(a)
+                        setValue2(b)
+                    }}
+
                     // сделать так чтоб value1 и value2 изменялось
                 />
-                <span>{value2}</span>
             </div>
 
             <hr/>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React from 'react';
+import ReactSlider from "react-slider";
+import './SuperDoubleRange.css';
+
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+    onChangeRange: (value: number[]) => void
+    value: [number, number]
 }
 
-const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
-    {
-        onChangeRange, value,
-        // min, max, step, disable, ...
-    }
-) => {
-    // сделать самому, можно подключать библиотеки
-
+const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = ({onChangeRange, value,}) => {
     return (
         <>
-            DoubleRange
+            <div>
+                <ReactSlider
+                    value={value}
+                    onChange={(value: number[]) => {
+                        onChangeRange(value)
+                    }}
+                    className="horizontal-slider"
+                    thumbClassName="example-thumb"
+                    trackClassName="example-track"
+                    defaultValue={[0, 100]}
+                    ariaLabel={['Lower thumb', 'Upper thumb']}
+                    ariaValuetext={state => `Thumb value ${state.valueNow}`}
+                    renderThumb={(props, state) => <div {...props}>{state.valueNow}</div>}
+                    pearling
+                    minDistance={5}
+                />
+            </div>
         </>
     )
 }
